11b22eaf2e53d815bbda24396c0411332ecb187d
Tridentinus <seaman.trent@gmail.com>
2020-07-16 22:02:59 -0400

    8 revised

--- 8_5.py
+++ 8_5.py
@@ -1,7 +1,13 @@
-fhand= open("mbox-short.txt")
-lines = 0
-for line in fhand:
-  if line.startswith("From "):
-    lines+=1
 
-print("There are",lines,"lines in the file with From as the first word")
+fileName = input("Enter the fileName:")
+fileData = open(fileName, 'r') 
+lines = fileData.readlines() 
+i=0 
+count=0
+while(i<len(lines)): 
+   words = lines[i].split(" ") 
+   if(words[0]=='From'): 
+       count=count+1 
+       print(words[1])
+   i=i+1
+print ("There were",count,"lines in the file with From as the first word")
