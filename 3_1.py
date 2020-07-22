6bdb72d6b6bb3091aa6f2abca6fc77cd3689fb99
Tridentinus <seaman.trent@gmail.com>
2020-07-22 12:59:56 -0400

    OK

--- 3_1.py
+++
@@ -1,8 +0,0 @@
-hours = float(input('Enter Hours:'))
-rate = float(input('Enter Rate:'))
-
-if hours <= 40:
-  pay = hours * rate
-else pay = ((hours - 40) * (1.5 * rate) + (40 * Rate)
-
-print ("Pay ="), pay
--- pay.py
+++
@@ -1,4 +0,0 @@
-hour = input("enter hours")
-rate = input("enter rate")
-pay = float(hour) * float(rate)
-print("Pay: ", Pay)
